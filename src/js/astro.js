bf128e3391ab35ace8db3e4c9577987dda863ae6
Chris Ferdinandi <hello@gomakethings.com>
2015-09-01 12:12:12 -0400

    Reverted to plugin approach

--- src/js/astro.js
+++ src/js/astro.js
@@ -1,45 +1,217 @@
-var astro = function ( toggle, menus, cb ) {
+(function (root, factory) {
+	if ( typeof define === 'function' && define.amd ) {
+		define([], factory(root));
+	} else if ( typeof exports === 'object' ) {
+		module.exports = factory(root);
+	} else {
+		root.astro = factory(root);
+	}
+})(typeof global !== 'undefined' ? global : this.window || this.global, function (root) {
 
 	'use strict';
 
+	//
 	// Variables
-	toggle = document.querySelector( toggle );
-	menus = document.querySelectorAll( menus );
+	//
 
-	// Sanity check
-	if ( !toggle || menus.length === 0 ) return;
+	var astro = {}; // Object for public APIs
+	var settings;
 
-	// Show and hide navigation menu
-	var toggleNav = function () {
+	// Default settings
+	var defaults = {
+		selector: '[data-nav-toggle]',
+		toggleActiveClass: 'active',
+		navActiveClass: 'active',
+		initClass: 'js-astro',
+		callback: function () {}
+	};
 
-		// Toggle the '.active' class on the toggle element
-		toggle.classList.toggle( 'active' );
 
-		for (var i = 0, len = menus.length; i < len; i++) {
-			menus[i].classList.toggle( 'active' );
+	//
+	// Methods
+	//
+
+	/**
+	 * Merge defaults with user options
+	 * @private
+	 * @param {Object} defaults Default settings
+	 * @param {Object} options User options
+	 * @returns {Object} Merged values of defaults and options
+	 */
+	var extend = function () {
+
+		// Variables
+		var extended = {};
+		var deep = false;
+		var i = 0;
+		var length = arguments.length;
+
+		// Check if a deep merge
+		if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
+			deep = arguments[0];
+			i++;
 		}
 
-		// Run callback
-		if ( cb && typeof(cb) === 'function' ) {
-			cb(toggle, pws);
+		// Merge the object into the extended object
+		var merge = function (obj) {
+			for ( var prop in obj ) {
+				if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
+					// If deep merge and property is an object, merge properties
+					if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' ) {
+						extended[prop] = buoy.extend( true, extended[prop], obj[prop] );
+					} else {
+						extended[prop] = obj[prop];
+					}
+				}
+			}
+		};
+
+		// Loop through each object and conduct a merge
+		for ( ; i < length; i++ ) {
+			var obj = arguments[i];
+			merge(obj);
 		}
+
+		return extended;
 
 	};
 
-	// Add class to <html> element on load
-	document.documentElement.classList.add( 'astro' );
+	/**
+	 * Get the closest matching element up the DOM tree
+	 * @private
+	 * @param {Element} elem Starting element
+	 * @param {String} selector Selector to match against (class, ID, or data attribute)
+	 * @return {Element} Returns null if no match found
+	 */
+	var getClosest = function ( elem, selector ) {
 
-	// Listen for menu clicks
-	toggle.addEventListener('click', function () {
+		// Variables
+		var firstChar = selector.charAt(0);
+		var attribute, value;
 
-		// Prevent default click event
-		if ( toggle.tagName.toLowerCase() === 'a' ) {
-			event.preventDefault();
+		// If selector is a data attribute, split attribute from value
+		if ( firstChar === '[' ) {
+			selector = selector.substr(1, selector.length - 2);
+			attribute = selector.split( '=' );
+
+			if ( attribute.length > 1 ) {
+				value = true;
+				attribute[1] = attribute[1].replace( /"/g, '' ).replace( /'/g, '' );
+			}
 		}
 
-		// Toggle nav
-		toggleNav();
+		// Get closest match
+		for ( ; elem && elem !== document; elem = elem.parentNode ) {
 
-	}, false); // Listen for click events and run event handler
+			// If selector is a class
+			if ( firstChar === '.' ) {
+				if ( elem.classList.contains( selector.substr(1) ) ) {
+					return elem;
+				}
+			}
 
-};
+			// If selector is an ID
+			if ( firstChar === '#' ) {
+				if ( elem.id === selector.substr(1) ) {
+					return elem;
+				}
+			}
+
+			// If selector is a data attribute
+			if ( firstChar === '[' ) {
+				if ( elem.hasAttribute( attribute[0] ) ) {
+					if ( value ) {
+						if ( elem.getAttribute( attribute[0] ) === attribute[1] ) {
+							return elem;
+						}
+					} else {
+						return elem;
+					}
+				}
+			}
+
+			// If selector is a tag
+			if ( elem.tagName.toLowerCase() === selector ) {
+				return elem;
+			}
+
+		}
+
+		return null;
+
+	};
+
+	/**
+	 * Show and hide navigation menu
+	 * @public
+	 * @param  {Element} toggle Element that triggered the toggle
+	 * @param  {String} navID The ID of the navigation element to toggle
+	 * @param  {Object} settings
+	 * @param  {Event} event
+	 */
+	astro.toggleNav = function ( toggle, navID, options, event ) {
+
+		// Selectors and variables
+		var settings = extend( settings || defaults, options || {} );  // Merge user options with defaults
+		var nav = document.querySelector(navID);
+
+		toggle.classList.toggle( settings.toggleActiveClass ); // Toggle the '.active' class on the toggle element
+		nav.classList.toggle( settings.navActiveClass ); // Toggle the '.active' class on the menu
+		settings.callback( toggle, navID ); // Run callbacks after toggling nav
+
+	};
+
+	/**
+	 * Handle click event methods
+	 * @private
+	 */
+	var eventHandler = function (event) {
+		var toggle = getClosest(event.target, settings.selector);
+		if ( toggle ) {
+			// Prevent default click event
+			if ( toggle.tagName.toLowerCase() === 'a') {
+				event.preventDefault();
+			}
+			// Toggle nav
+			astro.toggleNav( toggle, toggle.getAttribute('data-nav-toggle'), settings );
+		}
+	};
+
+	/**
+	 * Destroy the current initialization.
+	 * @public
+	 */
+	astro.destroy = function () {
+		if ( !settings ) return;
+		document.documentElement.classList.remove( settings.initClass );
+		document.removeEventListener('click', eventHandler, false);
+		settings = null;
+	};
+
+	/**
+	 * Initialize Astro
+	 * @public
+	 * @param {Object} options User settings
+	 */
+	astro.init = function ( options ) {
+
+		// Destroy any existing initializations
+		astro.destroy();
+
+		// Selectors and variables
+		settings = extend( defaults, options || {} ); // Merge user options with defaults
+
+		// Listeners and methods
+		document.documentElement.classList.add( settings.initClass ); // Add class to HTML element to activate conditional CSS
+		document.addEventListener('click', eventHandler, false); // Listen for click events and run event handler
+
+	};
+
+
+	//
+	// Public APIs
+	//
+
+	return astro;
+
+});
---
+++ test/spec/spec-astro.js
@@ -0,0 +1,219 @@
+describe('Astro', function () {
+
+	//
+	// Helper Functions
+	//
+
+	/**
+	 * Inserts Houdini markup into DOM
+	 */
+	var injectElem = function () {
+		var elem =
+			'<nav class="nav-wrap nav-collapse">' +
+				'<a class="logo" href="#">My Brand</a>' +
+				'<a class="nav-toggle" data-nav-toggle="#nav-menu" href="#">Menu</a>' +
+				'<div class="nav-menu" id="nav-menu">' +
+					'<ul class="nav">' +
+						'<li><a href="#">Home</a></li>' +
+						'<li><a href="#">About</a></li>' +
+					'</ul>' +
+				'</div>' +
+			'</nav>';
+		document.body.innerHTML = elem;
+	};
+
+	/**
+	 * Triggers an event
+	 * @param  {String} type Type of event (ex. 'click')
+	 * @param  {Element} elem The element that triggered the event
+	 * @link http://stackoverflow.com/a/2490876
+	 */
+	var trigger = function (type, elem) {
+		var event; // The custom event that will be created
+
+		if (document.createEvent) {
+			event = document.createEvent('HTMLEvents');
+			event.initEvent(type, true, true);
+		} else {
+			event = document.createEventObject();
+			event.eventType = type;
+		}
+
+		event.eventName = type;
+
+		if (document.createEvent) {
+			elem.dispatchEvent(event);
+		} else {
+			elem.fireEvent("on" + event.eventType, event);
+		}
+	};
+
+	/**
+	 * Bind polyfill for PhantomJS
+	 * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind#Compatibility
+	 */
+	if (!Function.prototype.bind) {
+		Function.prototype.bind = function (oThis) {
+			if (typeof this !== "function") {
+				// closest thing possible to the ECMAScript 5
+				// internal IsCallable function
+				throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+			}
+
+			var aArgs = Array.prototype.slice.call(arguments, 1);
+			var fToBind = this;
+			var fNOP = function () {};
+			var fBound = function () {
+				return fToBind.apply(this instanceof fNOP && oThis ? this : oThis, aArgs.concat(Array.prototype.slice.call(arguments)));
+			};
+
+			fNOP.prototype = this.prototype;
+			fBound.prototype = new fNOP();
+
+			return fBound;
+		};
+	}
+
+
+	//
+	// Init
+	//
+
+	describe('Should initialize plugin', function () {
+
+		beforeEach(function () {
+			astro.init();
+		});
+
+		it('Document should include the astro module', function () {
+			expect(!!astro).toBe(true);
+		});
+
+		it('Document should contain init class', function () {
+			expect(document.documentElement.classList.contains('js-astro')).toBe(true);
+		});
+
+	});
+
+	describe('Should merge user options into defaults', function () {
+
+		var toggle, nav, doc;
+
+		beforeEach(function () {
+			injectElem();
+			astro.init({
+				toggleActiveClass: 'toggle-active',
+				navActiveClass: 'nav-active',
+				initClass: 'js-test',
+				callback: function () { document.documentElement.classList.add('callback'); }
+			});
+			toggle = document.querySelector('[data-nav-toggle]');
+			nav = document.querySelector( toggle.getAttribute('data-nav-toggle') );
+			doc = document.documentElement;
+		});
+
+		it('User options should be merged into defaults', function () {
+			trigger('click', toggle);
+			expect(toggle.classList.contains('toggle-active')).toBe(true);
+			expect(nav.classList.contains('nav-active')).toBe(true);
+			expect(doc.classList.contains('js-test')).toBe(true);
+			expect(doc.classList.contains('callback')).toBe(true);
+		});
+
+	});
+
+
+	//
+	// Events
+	//
+
+	describe('Should hide and show navigation menu on click', function () {
+
+		var toggle, nav;
+
+		beforeEach(function () {
+			injectElem();
+			astro.init();
+			toggle = document.querySelector('[data-nav-toggle]');
+			nav = document.querySelector( toggle.getAttribute('data-nav-toggle') );
+		});
+
+		it('Toggle and nav should have ".active" class on click', function () {
+			trigger('click', toggle);
+			expect(toggle.classList.contains('active')).toBe(true);
+			expect(nav.classList.contains('active')).toBe(true);
+		});
+
+		it('Toggle and nav should not have ".active" class if toggle is clicked again', function () {
+			trigger('click', toggle);
+			expect(toggle.classList.contains('active')).toBe(true);
+			expect(nav.classList.contains('active')).toBe(true);
+			trigger('click', toggle);
+			expect(toggle.classList.contains('active')).toBe(false);
+			expect(nav.classList.contains('active')).toBe(false);
+		});
+
+	});
+
+
+	//
+	// APIs
+	//
+
+	describe('Should toggle navigation menu from public API', function () {
+
+		var toggle, navID, nav;
+
+		beforeEach(function () {
+			injectElem();
+			toggle = document.querySelector('[data-nav-toggle]');
+			navID = toggle.getAttribute('data-nav-toggle');
+			nav = document.querySelector( navID );
+			astro.toggleNav(toggle, navID, null, null);
+		});
+
+		it('Toggle and navigation menu should have an active class', function () {
+			expect(toggle.classList.contains('active')).toBe(true);
+			expect(nav.classList.contains('active')).toBe(true);
+		});
+
+		it('Toggle and navigation menu should not have an active class if toggled again', function () {
+			expect(toggle.classList.contains('active')).toBe(true);
+			expect(nav.classList.contains('active')).toBe(true);
+			astro.toggleNav(toggle, navID, null, null);
+			expect(toggle.classList.contains('active')).toBe(false);
+			expect(nav.classList.contains('active')).toBe(false);
+		});
+
+	});
+
+	describe('Should remove initialized plugin', function () {
+
+		var toggle, nav, doc;
+
+		beforeEach(function () {
+			injectElem();
+			astro.init();
+			toggle = document.querySelector('[data-nav-toggle]');
+			nav = document.querySelector( toggle.getAttribute('data-nav-toggle') );
+			doc = document.documentElement;
+		});
+
+		it('Astro should be uninitialized', function () {
+			trigger('click', toggle);
+			expect(toggle.classList.contains('active')).toBe(true);
+			expect(nav.classList.contains('active')).toBe(true);
+			expect(doc.classList.contains('js-astro')).toBe(true);
+			trigger('click', toggle);
+			expect(toggle.classList.contains('active')).toBe(false);
+			expect(nav.classList.contains('active')).toBe(false);
+			astro.destroy();
+			trigger('click', toggle);
+			expect(toggle.classList.contains('active')).toBe(false);
+			expect(nav.classList.contains('active')).toBe(false);
+			expect(doc.classList.contains('js-astro')).toBe(false);
+		});
+
+	});
+
+});
